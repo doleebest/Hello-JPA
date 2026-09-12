adb5483229bf750bbe0338b1a04cb7e6bb329d37
Sojeong Lee <sj27419@gmail.com>
2026-09-13 00:45:07 +0900

    feat: 영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,24 +13,11 @@ public class JpaMain {
         tx.begin();
 
         try{
-            // 비영속
-//            Member member = new Member() ;
-//            member.setId(101L);
-//            member.setName("Hellojpa");
 
-            // 영속
-//            System.out.println("before");
-//            em.persist(member);
-//            System.out.println("after");
+            Member member1 = em.find(Member.class, 150L);
+            member1.setName("zzzz");
 
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
-
-            System.out.println(findMember1 == findMember2);
-
-//            System.out.println(findMember1.getId());
-//            System.out.println(findMember2.getId());
-//            System.out.println(findMember.getName());
+            System.out.println("-------------");
 
             tx.commit();
         } catch (Exception e){
