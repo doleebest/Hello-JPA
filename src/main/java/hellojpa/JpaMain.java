872c2365466322c198d79e22c7c88a8be0701e4f
Sojeong Lee <sj27419@gmail.com>
2026-09-13 00:31:29 +0900

    feat: 영속성 컨텍스트 2 코드가 많이 바뀌어서 커밋 ~

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,14 +13,25 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member = new Member() ;
-            member.setId(100L);
-            member.setName("Hellojpa");
+            // 비영속
+//            Member member = new Member() ;
+//            member.setId(101L);
+//            member.setName("Hellojpa");
 
             // 영속
-            System.out.println("before");
-            em.persist(member);
-            System.out.println("before");
+//            System.out.println("before");
+//            em.persist(member);
+//            System.out.println("after");
+
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            System.out.println(findMember1 == findMember2);
+
+//            System.out.println(findMember1.getId());
+//            System.out.println(findMember2.getId());
+//            System.out.println(findMember.getName());
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
