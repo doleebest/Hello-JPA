d813c8e30373e9698b45611bbc47e0df6bbc7f02
Sojeong Lee <sj27419@gmail.com>
2026-09-12 23:51:39 +0900

    feature: 영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,7 +8,23 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member() ;
+            member.setId(100L);
+            member.setName("Hellojpa");
+
+            // 영속
+            System.out.println("before");
+            em.persist(member);
+            System.out.println("before");
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        }
 
         em.close();
         emf.close();
