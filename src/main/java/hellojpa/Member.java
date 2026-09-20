0e5cc589cbcb815705366857c866bd0c0e30b5c9
Sojeong Lee <sj27419@gmail.com>
2026-09-20 22:01:34 +0900

    feat: mappedby = 연관관계의 주인쪽에서 나를 가리키는 필드명

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -22,7 +22,7 @@ public class Member {
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
 
-    @OneToMany (mappedBy = "product")
+    @OneToMany (mappedBy = "member")
     private List<MemberProduct> memberProducts = new ArrayList<>();
     public Member() {
     }
