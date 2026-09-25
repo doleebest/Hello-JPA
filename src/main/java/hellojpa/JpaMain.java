84ecc59315ee2d558ec0eb444acd1b2846b110f5
Sojeong Lee <sj27419@gmail.com>
2026-09-26 00:13:00 +0900

    feat: 값 타입 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -14,17 +16,29 @@ public class JpaMain {
 
         try{
 
-            Address address = new Address("CITY", "STREET" ,"ZIPCODE");
-
+            Address address = new Address("homeCity", "STREET" ,"ZIPCODE");
             Member member = new Member();
             member.setName("member1");
             member.setHomeAddress(address);
+
+            member.getFavoriteFoods().add("chicken");
+            member.getFavoriteFoods().add("족발");
+
+            member.getAddressHistory().add(new Address("old1","street","10000"));
+            member.getAddressHistory().add(new Address("old2","street","10000"));
             em.persist(member);
 
-            Address address2 = new Address(address.getCity(), address.getStreet(), address.getZipcode());
-            member.setHomeAddress(address2); // 통으로 다 reset
+            em.flush();
+            em.clear();
 
-//            member.getHomeAddress().setCity("new new");
+            System.out.println("-----");
+            Member findMember = em.find(Member.class, member.getId());
+
+
+            List<Address> addresses = findMember.getAddressHistory();
+            for (Address a : addresses){
+                System.out.println(a.getCity());
+            }
 
             tx.commit();
         } catch (Exception e){
